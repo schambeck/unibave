67a1dc0f06beb0ac3eb3b751376982bbce11503f
Marcelo Schambeck <mschambeck@gmail.com>
2016-04-08 20:44:00 -0300

    Aulas

--- android/05-ParametrosActivity/app/src/main/java/net/unibave/parametrosactivity/MainActivity.java
+++ android/05-ParametrosActivity/app/src/main/java/net/unibave/parametrosactivity/MainActivity.java
@@ -1,10 +1,11 @@
-package net.unibave.parametrosactivity;
+package unibave.net.parametrosactivity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,14 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         etxAluno = (EditText) findViewById(R.id.etxAluno);
     }
 
-    public void btnOkOnClick(View view) {
-        String aluno = etxAluno.getText().toString();
+    public void btnOKOnClick(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(KEY, aluno);
-        startActivity(intent);
+        intent.putExtra(KEY, etxAluno.getText().toString());
+//        startActivity(intent);
+        startActivityForResult(intent, 1);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String idade = data.getStringExtra(SecondActivity.KEY);
+            Toast.makeText(this, etxAluno.getText().toString() + " tem " + idade + " anos!", Toast.LENGTH_LONG).show();
+        }
+    }
 }
